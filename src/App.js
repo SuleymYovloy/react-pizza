812dc4d27acceb9094bb9ef959d6e9e80384a69a
Suleym <suleymevlo81@gmail.com>
2022-12-26 00:14:54 +0300

    Добавил массив с пиццами. Сделал категории выбора пиццы.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 import './scss/app.scss';
 
 function App() {
@@ -18,10 +20,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            <PizzaBlock title="Четыре-сыра" price="620" />
-            <PizzaBlock title="Мексиканская-острая" price="560" />
-            <PizzaBlock title="Римская" price="430" />
-            <PizzaBlock title="Сицилийская" price="510" />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj} />
+            ))}
           </div>
         </div>
       </div>
